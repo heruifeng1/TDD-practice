7f01632c7cb1c1a7b01716173f94d9b59427a144
JimmyLv <jimmy.jinglv@gmail.com>
2020-01-29 17:37:28 +0800

    fix: added error status and validate invalid string

--- src/components/FizzBuzz.test.js
+++ src/components/FizzBuzz.test.js
@@ -15,6 +15,7 @@ describe('FizzBuzz UI', () => {
     const { queryByTestId } = render(<FizzBuzz />)
 
     userEvent.type(queryByTestId('input-number'), '2')
+    userEvent.click(queryByTestId('check-button'))
 
     const result = queryByTestId('number-result').textContent
     expect(result).toEqual('2')
@@ -23,6 +24,7 @@ describe('FizzBuzz UI', () => {
     const { queryByTestId } = render(<FizzBuzz />)
 
     userEvent.type(queryByTestId('input-number'), '3')
+    userEvent.click(queryByTestId('check-button'))
 
     const result = queryByTestId('number-result').textContent
     expect(result).toEqual('Fizz')
@@ -31,6 +33,7 @@ describe('FizzBuzz UI', () => {
     const { queryByTestId } = render(<FizzBuzz />)
 
     userEvent.type(queryByTestId('input-number'), '5')
+    userEvent.click(queryByTestId('check-button'))
 
     const result = queryByTestId('number-result').textContent
     expect(result).toEqual('Buzz')
@@ -39,16 +42,26 @@ describe('FizzBuzz UI', () => {
     const { queryByTestId } = render(<FizzBuzz />)
 
     userEvent.type(queryByTestId('input-number'), '15')
+    userEvent.click(queryByTestId('check-button'))
 
     const result = queryByTestId('number-result').textContent
     expect(result).toEqual('FizzBuzz')
   })
-  it('should show error message when input invalid number', () => {
+  it('should show error message when input number out of scope', () => {
     const { queryByTestId } = render(<FizzBuzz />)
 
     userEvent.type(queryByTestId('input-number'), '101')
 
-    const result = queryByTestId('number-result').textContent
+    const result = queryByTestId('error-result').textContent
     expect(result).toEqual('invalid input: 101')
+  })
+
+  it('should show error message when input invalid string', () => {
+    const { queryByTestId } = render(<FizzBuzz />)
+
+    userEvent.type(queryByTestId('input-number'), 'error')
+
+    const result = queryByTestId('error-result').textContent
+    expect(result).toEqual('invalid input: error')
   })
 })
