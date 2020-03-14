76f824c30453bffcf0882498fa5154c5d476660f
JimmyLv <jimmy.jinglv@gmail.com>
2020-03-14 16:43:02 +0800

    feat: update the start code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { FizzBuzz } from './components/FizzBuzz'
 
 export default function App() {
   return (
     <>
       <h1 data-testid="title">练功房前端脚手架</h1>
-      <FizzBuzz />
     </>
   )
 }
